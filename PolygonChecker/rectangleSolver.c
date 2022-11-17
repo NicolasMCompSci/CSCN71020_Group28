8f376448ed7e47c6d054a263fc622eabe93f9b9b
grae <gwatson2156@conestogac.on.ca>
2022-11-17 17:06:32 -0500

    adding check square function

--- PolygonChecker/rectangleSolver.c
+++ PolygonChecker/rectangleSolver.c
@@ -1,5 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "rectangleSolver.h"
 
 // CSCN71020 - fall 2022 
 // Group 28 - Nicolas Masquieto, Grae Watson, Zavere Vidal
 // 
 // Implementation for Rectangle Solver functions
+
+bool checkIfSquareByPoints(int pointOneX, int pointOneY, int pointTwoX, int pointTwoY, int pointThreeX, int pointThreeY, int pointFourX, int pointFourY)
+{
+	// this only works if point 1 is bottom left point point 2 is top left, point 3 top right, point 4 bottom right
+	if (pointOneX != pointTwoX || pointTwoY != pointThreeY || pointThreeX != pointFourX || pointFourY != pointOneY)
+		return false;
+	else
+		return true;
+}
--- PolygonChecker/rectangleSolver.h
+++ PolygonChecker/rectangleSolver.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <stdbool.h>
+
 // CSCN71020 - fall 2022 
 // Group 28 - Nicolas Masquieto, Grae Watson, Zavere Vidal
 // 
 // Interface for Rectangle Solver functions
+
+bool checkIfSquareByPoints(int, int, int, int, int, int, int, int);
