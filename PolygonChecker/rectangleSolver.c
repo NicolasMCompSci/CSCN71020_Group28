1e709017e1a81e7702a7c5cb315de1823d8b5986
NicolasMCompSci <115744859+NicolasMCompSci@users.noreply.github.com>
2022-11-23 15:44:36 -0500

    created functions to get rectangle side lengths, perimeter of rectangle, and area of rectangle

--- PolygonChecker/rectangleSolver.c
+++ PolygonChecker/rectangleSolver.c
@@ -1,7 +1,9 @@
+#include "rectangleSolver.h"
+
 #include <stdio.h>
 #include <stdbool.h>
-
-#include "rectangleSolver.h"
+#include <stdarg.h>
+#include <math.h>
 
 // CSCN71020 - fall 2022 
 // Group 28 - Nicolas Masquieto, Grae Watson, Zavere Vidal
@@ -17,8 +19,46 @@ bool checkIfRectangleByPoints(int pointOneX, int pointOneY, int pointTwoX, int p
 		return true;
 }
 
+void getRectangleSidesFromPoints(float result[], POINT points[], int arrSize)
+{
+	// find array index max for array iterations
+	int maxArrIndex = arrSize - 1;
+	
+	// iterates through the arrays and populates result[] with the side lengths
+	for (int i = 0; i < maxArrIndex; i++)
+	{
+		int next = i + 1;
+		float xLen = points[next].x - points[i].x;
+		float yLen = points[next].y - points[i].y;
+		result[i] = sqrt((xLen * xLen) + (yLen * yLen));
+	}
+
+	// special calculation needed for last line
+	float xLen = points[maxArrIndex].x - points[0].x;
+	float yLen = points[maxArrIndex].y - points[0].y;
+	result[maxArrIndex] = sqrt((xLen * xLen) + (yLen * yLen));
+}
+
+float getFloatPerimeterFromSides(float sideLengths[], int arrSize)
+{
+	// declare and initialize perimeter to 0 for safety
+	float perimeter = 0;
+	// sums the length of all sides to get perimeter
+	for (int i = 0; i < arrSize; i++)
+		perimeter += sideLengths[i];
+
+	return perimeter;
+}
+
+float getRectangleArea(float sideLengths[], int size)
+{
+	// returns the product between the first and second side lengths
+	return sideLengths[0] * sideLengths[1];
+}
+
 char* getRectangleResultForPrint(bool isRectangle)
 {
+	// declare and initialize result to empty string
 	char* result = "";
 
 	if (isRectangle)
@@ -28,7 +68,6 @@ char* getRectangleResultForPrint(bool isRectangle)
 
 	return result;
 }
-
 
 // zavere
 //char* analyzeRectangle(int side1, int side2, int side3, int side4) {   
