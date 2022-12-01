e8ed2182afcc0ad409f70769b64dea9df02efcfb
NicolasMCompSci <115744859+NicolasMCompSci@users.noreply.github.com>
2022-12-01 16:26:53 -0500

    updated 4 points test

--- FourPointsFunctionality_Test/FourPointsFunctionality_Test.cpp
+++ FourPointsFunctionality_Test/FourPointsFunctionality_Test.cpp
@@ -8,6 +8,7 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace FourPointsFunctionalityTest
 {
+	// test cases for "4 points" functionality
 	TEST_CLASS(FourPointsFunctionalityTest)
 	{
 	public:
@@ -23,13 +24,35 @@ namespace FourPointsFunctionalityTest
 			Assert::AreEqual(true , result);
 		}
 
+		TEST_METHOD(Functional02)
+		{
+			// functional test in case rectangle is created counter-clockwise
+			POINT first; first.x = 3; first.y = 0;
+			POINT second; second.x = 3; second.y = 1;
+			POINT third; third.x = 1; third.y = 1;
+			POINT fourth; fourth.x = 1; fourth.y = 0;
+			bool result = checkIfRectangleByPoints(first, second, third, fourth);
+			Assert::AreEqual(true, result);
+		}
+
+		TEST_METHOD(Functional03)
+		{
+			// test when rectangle points are all negative
+			POINT first; first.x = -100; first.y = -100;
+			POINT second; second.x = -100; second.y = -300;
+			POINT third; third.x = -200; third.y = -300;
+			POINT fourth; fourth.x = -200; fourth.y = -100;
+			bool result = checkIfRectangleByPoints(first, second, third, fourth);
+			Assert::AreEqual(true, result);
+		}
+
 		TEST_METHOD(Boundary01)
 		{
 			// tests values for biggest possible rectangle
-			POINT first; first.x = 0; first.y = 0;
-			POINT second; second.x = 0; second.y = 2147483647;
+			POINT first; first.x = -2147483648; first.y = -2147483648;
+			POINT second; second.x = -2147483648; second.y = 2147483647;
 			POINT third; third.x = 2147483647; third.y = 2147483647;
-			POINT fourth; fourth.x = 2147483647; fourth.y = 0;
+			POINT fourth; fourth.x = 2147483647; fourth.y = -2147483648;
 			bool result = checkIfRectangleByPoints(first, second, third, fourth);
 			Assert::AreEqual(true, result);
 		}
