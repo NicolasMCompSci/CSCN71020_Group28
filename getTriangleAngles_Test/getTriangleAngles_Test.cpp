619ccc4bbbee0924660ca12a10bd9efcbc141115
grae <gjw029@gmail.com>
2022-12-10 16:46:43 -0500

    added test suit for testing assignTriangleAngles

---
+++ getTriangleAngles_Test/getTriangleAngles_Test.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern "C" void assignTriangleAngles(int triangleSides[], double triangleAngles[]);
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace getTriangleAnglesTest
+{
+	TEST_CLASS(getTriangleAnglesTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+}
